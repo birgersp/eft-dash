347a2cd55a5bd9dff8545ac436bdb72757f2308d
Birger Skogeng Pedersen <birger.sp@gmail.com>
2019-11-10 22:48:00 +0100

    images now maintain aspect ratio, fixed title

--- web/script.js
+++ web/script.js
@@ -6,12 +6,15 @@ const mapURLs = {
 	shoreline: "https://forum.escapefromtarkov.com/uploads/monthly_2019_01/shoreline_marvelin_2_2.jpg.496b8c902f7b88b12d474fd3107ce578.jpg",
 	factory: "https://forum.escapefromtarkov.com/uploads/monthly_2018_02/Factory_marvelin1_2.jpg.0c4c03b58ecfff4b1fe15afef5291e97.jpg",
 	dorms_customs: "https://forum.escapefromtarkov.com/uploads/monthly_2018_02/Doorms_marvelin1_2.thumb.jpg.46d00383a0269b37daeb1a3457cca03c.jpg",
-	health_resort_shoreline: "https://forum.escapefromtarkov.com/uploads/monthly_2018_02/spa_marvelin1_1.jpg.e192f88f3ba73bccdcb437185a44d1d5.jpg"
+	health_resort_shoreline: "https://forum.escapefromtarkov.com/uploads/monthly_2018_02/spa_marvelin1_1.jpg.e192f88f3ba73bccdcb437185a44d1d5.jpg",
+	interchange: "https://i.redd.it/bqftzweimvx31.png"
 }
 
 let app = {
+	title: "EFT Stuff",
 	image: null,
-	indexedMapUrls: {}
+	indexedMapUrls: {},
+	headerHeight: "20px",
 }
 
 function setStyle(element, style) {
@@ -52,9 +55,14 @@ function setImage(url) {
 
 function main() {
 
+	let titleElement = appendElement(document.head, "title");
+	setAttributes(titleElement, { innerHTML: app.title });
+
 	setStyle(document.body, {
 		margin: "0",
-		background: "#17202A"
+		background: "#17202A",
+		"overflow-y": "hidden",
+		"margin-bottom": "20px"
 	});
 
 	let headerDiv = appendElement(document.body, "div", {
@@ -90,11 +98,13 @@ function main() {
 	setAttributes(text, { innerHTML: "Tip: Use hotkeys. Press F11 to fullscreen" });
 
 	let imageDiv = appendElement(document.body, "div");
-	app.image = appendElement(imageDiv, "img");
-	setAttributes(app.image, {
-		src: mapURLs.customs,
+	app.image = appendElement(imageDiv, "img", {
 		width: "100%",
-		height: "97%"
+		height: "100%",
+		"object-fit": "contain"
+	});
+	setAttributes(app.image, {
+		src: mapURLs.customs
 	});
 
 	window.addEventListener("keypress", (event) => {
@@ -103,3 +113,4 @@ function main() {
 		}
 	});
 }
+
