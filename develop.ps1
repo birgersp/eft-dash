7ae6c879ddca6eaa3cdf86db329330d4d02c2f8c
Birger Skogeng Pedersen <birger.sp@gmail.com>
2020-12-04 18:11:21 +0100

    begin v2

---
+++ develop.ps1
@@ -0,0 +1,3 @@
+invoke-expression 'cmd /c start powershell -Command { tsc --watch }'
+invoke-expression 'cmd /c start powershell -Command { gulp }'
+invoke-expression 'cmd /c start powershell -Command { ./node_modules/.bin/http-server dist -p 8000 -o }'
