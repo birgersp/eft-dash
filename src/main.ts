9850a1117f3c73cc09462c6e5c1e67141010fbec
Birger Skogeng Pedersen <birger.sp@gmail.com>
2020-12-13 18:32:52 +0100

    add features

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,42 @@
-import { setAttributes } from "./util"
+import { clearDocument, setAttributes, setStyle } from "./util"
 
 let TIMESTAMP = ""
 // @echo TIMESTAMP
 console.log(`Built: ${TIMESTAMP}`)
 
-let createElement = document.createElement
+let globals = {
+	RESIZE_TIMER_MS: 100
+}
 
 export function main() {
-	let input = createElement("input")
-	setAttributes(input, {
-		"type": "button"
+
+	clearDocument()
+
+	setStyle(document.body, {
+		"margin": "0px",
+		"overflow": "hidden"
 	})
-	document.body.appendChild(input)
+
+	let canvas = document.createElement("canvas")
+	setAttributes(canvas, {
+
+	})
+	setStyle(canvas, {
+
+	})
+	document.body.appendChild(canvas)
+
+	function onResize() {
+		canvas.width = document.body.clientWidth
+		canvas.height = document.body.clientHeight
+	}
+
+	onResize()
+
+	let timeout = 0
+	window.addEventListener("resize", () => {
+		window.clearTimeout(timeout)
+		timeout = window.setTimeout(onResize, globals.RESIZE_TIMER_MS)
+	})
 }
 window.addEventListener("load", () => { main() })
--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,23 @@
 export function setAttributes(element: HTMLElement, properties: any) {
+
 	for (let key in properties) {
 		element.setAttribute(key, properties[key])
+	}
+}
+
+export function setStyle(element: HTMLElement, properties: any) {
+
+	for (let key in properties) {
+		element.style.setProperty(key, properties[key])
+	}
+}
+
+export function clearDocument() {
+
+	let body = document.body
+	let child = body.lastChild
+	while (child != null) {
+		body.removeChild(child)
+		child = body.lastChild
 	}
 }
