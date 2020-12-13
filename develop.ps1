3784f585fc8ca7acbfd704abfeacd8dcd23992c2
Birger Skogeng Pedersen <birger.sp@gmail.com>
2020-12-13 18:32:43 +0100

    remove develop script

--- develop.ps1
+++
@@ -1,3 +0,0 @@
-invoke-expression 'cmd /c start powershell -Command { tsc --watch }'
-invoke-expression 'cmd /c start powershell -Command { gulp }'
-invoke-expression 'cmd /c start powershell -Command { ./node_modules/.bin/http-server dist -p 8000 -o }'
