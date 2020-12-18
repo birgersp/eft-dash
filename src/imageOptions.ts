361394dda6aaf10f97cfb7074b97690027695dd1
Birger Skogeng Pedersen <birger.sp@gmail.com>
2020-12-18 20:36:10 +0100

    add imageOptions

---
+++ src/imageOptions.ts
@@ -0,0 +1,86 @@
+type ImageOption_temp = {
+	authorName: string,
+	hotkey: string,
+	image: string,
+	name: string,
+	sourceUrl: string
+}
+let imageOptions: ImageOption_temp[] = []
+
+imageOptions.push({
+	authorName: "u/monkimonkimonk",
+	hotkey: "1",
+	image: "https://i.redd.it/8udfaht87zd51.png",
+	name: "Customs",
+	sourceUrl: "https://www.reddit.com/r/EscapefromTarkov/comments/i0k2qz/updated_customs_map_for_127_with_loot_caches/"
+})
+imageOptions.push({
+	authorName: "Marvelin",
+	hotkey: "2",
+	image: "https://forum.escapefromtarkov.com/uploads/monthly_2018_02/woods_marvelin.jpg.d4692fbf57cdfd608671b16f8caf89ae.jpg",
+	name: "Woods",
+	sourceUrl: "https://forum.escapefromtarkov.com/topic/56652-maps-of-tarkov"
+})
+imageOptions.push({
+	authorName: "Marvelin",
+	hotkey: "3",
+	image: "https://forum.escapefromtarkov.com/uploads/monthly_2019_01/shoreline_marvelin_2_2.jpg.496b8c902f7b88b12d474fd3107ce578.jpg",
+	name: "Shoreline",
+	sourceUrl: "https://forum.escapefromtarkov.com/topic/56652-maps-of-tarkov"
+})
+imageOptions.push({
+	authorName: "Marvelin",
+	hotkey: "4",
+	image: "https://forum.escapefromtarkov.com/uploads/monthly_2018_02/Factory_marvelin1_2.jpg.0c4c03b58ecfff4b1fe15afef5291e97.jpg",
+	name: "Factory",
+	sourceUrl: "https://forum.escapefromtarkov.com/topic/56652-maps-of-tarkov"
+})
+imageOptions.push({
+	authorName: "Marvelin",
+	hotkey: "5",
+	image: "https://forum.escapefromtarkov.com/uploads/monthly_2018_02/Doorms_marvelin1_2.thumb.jpg.46d00383a0269b37daeb1a3457cca03c.jpg",
+	name: "Dorms, Customs",
+	sourceUrl: "https://forum.escapefromtarkov.com/topic/56652-maps-of-tarkov"
+})
+imageOptions.push({
+	authorName: "Marvelin",
+	hotkey: "6",
+	image: "https://forum.escapefromtarkov.com/uploads/monthly_2018_02/spa_marvelin1_1.jpg.e192f88f3ba73bccdcb437185a44d1d5.jpg",
+	name: "Resort, Shoreline",
+	sourceUrl: "https://forum.escapefromtarkov.com/topic/56652-maps-of-tarkov"
+})
+imageOptions.push({
+	authorName: "u/Lorathor6",
+	hotkey: "7",
+	image: "https://i.redd.it/bqftzweimvx31.png",
+	name: "Interchange",
+	sourceUrl: "https://www.reddit.com/r/EscapefromTarkov/comments/8gixyg/interchange_map_incl_loot_and_keys"
+})
+imageOptions.push({
+	authorName: "Pestily",
+	hotkey: "k",
+	image: "https://i.imgur.com/WI3Qg2G.jpg",
+	name: "Keys",
+	sourceUrl: "https://www.reddit.com/r/EscapefromTarkov/comments/f3tfry/pestilys_recommended_keys_for_every_map"
+})
+imageOptions.push({
+	authorName: "sunmachine",
+	hotkey: "a",
+	image: "https://i.redd.it/eawzvoj4blc41.png",
+	name: "Ammo",
+	sourceUrl: "https://www.reddit.com/r/EscapefromTarkov/comments/eqk10n/for_new_players_my_braindead_simple_ammo_chart/"
+})
+imageOptions.push({
+	authorName: "u/Fenneca",
+	hotkey: "i",
+	image: "https://i.redd.it/bu31y4cnikh41.png",
+	name: "Items",
+	sourceUrl: "https://www.reddit.com/r/EscapefromTarkov/comments/f5htgo/i_made_a_quick_glance_per_slot_value_guide_for/fi3z8rz/"
+})
+imageOptions.push({
+	authorName: "u/Chab_TV",
+	hotkey: "h",
+	image: "https://gamepedia.cursecdn.com/escapefromtarkov_gamepedia/3/39/Hideout-Requirements-Items-to-Keep.jpg",
+	name: "Hideout Items",
+	sourceUrl: "https://www.reddit.com/r/EscapefromTarkov/comments/du957n/hideout_requirements_items_to_keep/"
+})
